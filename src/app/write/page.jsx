ca1889f679d10707dbdc9ba668a8a0142c377824
Sulaimon Omotosho <susudef2u@gmail.com>
2023-11-03 10:51:17 +0100

    Unknown mistake fixed on Write Page

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -24,7 +24,7 @@ const WritePage = () => {
             </button>
             <button className={styles.addButton}>
               <Image src='/external.png' alt='' width={16} height={16} />
-            </button>{' '}
+            </button>
             <button className={styles.addButton}>
               <Image src='/video.png' alt='' width={16} height={16} />
             </button>
